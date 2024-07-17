6ab5a5bfb0e24f0b46bc8f0e18af3098bf8f2344
Denys Polishchuk <polischyk0000@gmail.com>
2024-07-17 15:01:42 +0300

    create component ContactForm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,14 @@
+import ContactForm from "../ContactForm/ContactForm";
+
 import style from "./App.module.css";
 
 function App() {
-  return <div className={style.app}></div>;
+  return (
+    <div className={style.app}>
+      <h1 className={style.appTitle}>Phonebook</h1>
+      <ContactForm />
+    </div>
+  );
 }
 
 export default App;
